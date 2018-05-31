b6149e920865158458a0f305f84e9a7f1647532b
Richard Cho <richardcho100@gmail.com>
2018-05-31 21:01:01 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,12 +13,7 @@ class Triangle
     elsif @side_1 != @side_2 && @side_1 != @side_3 && @side_2 != @side_3
       :scalene
     elsif @side_1 == 0 || @side_1 < 0 || @side_1 + @side_2 <= @side_3 || @side_2 + @side_3 <= @side_1 || @side_3 +@side_1 <= @side_2
-    #@side_1 == 0 && @side_2 == 0 && @side_3 == 0
-      begin
-        raise TriangleError
-      rescue TriangleError
-
-      end
+      raise TriangleError
     end
   end
 
@@ -26,3 +21,5 @@ class Triangle
 
   end
 end
+
+#@side_1 == 0 && @side_2 == 0 && @side_3 == 0
